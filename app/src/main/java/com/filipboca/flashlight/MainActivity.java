33e0197a5727529f5e869831aaa6d0e69e2cf101
fboca <47888704+fboca@users.noreply.github.com>
2022-07-02 22:24:12 +0300

    Flashlight 1.0
    Simple Flashlight mobile application for android devices.

---
+++ app/src/main/java/com/filipboca/flashlight/MainActivity.java
@@ -0,0 +1,102 @@
+package com.filipboca.flashlight;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
+import android.os.Build;
+import android.view.View;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ToggleButton toggleFlashLightOnOff;
+    private CameraManager cameraManager;
+    private String getCameraID;
+    private AdView mAdView;
+
+    @SuppressLint("ResourceAsColor")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+
+        // Register the ToggleButton with specific ID
+        toggleFlashLightOnOff = findViewById(R.id.toggle_flashlight);
+
+        // cameraManager to interact with camera devices
+        cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+
+        //Get the main container and set black color
+        ConstraintLayout maincontainer = findViewById(R.id.main_container);
+        maincontainer.setBackgroundColor(R.color.black);
+
+        // Exception is handled, because to check whether
+        // the camera resource is being used by another
+        // service or not.
+        try {
+            // O means back camera unit,
+            // 1 means front camera unit
+            getCameraID = cameraManager.getCameraIdList()[0];
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public void toggleFlashLight(View view) {
+        if (toggleFlashLightOnOff.isChecked()) {
+            // Exception is handled, because to check
+            // whether the camera resource is being used by
+            // another service or not.
+            try {
+                // true sets the torch in ON mode
+                cameraManager.setTorchMode(getCameraID, true);
+
+                // Inform the user about the flashlight
+                // status using Toast message
+                Toast.makeText(MainActivity.this, "Flashlight is turned ON", Toast.LENGTH_SHORT).show();
+            } catch (CameraAccessException e) {
+                // prints stack trace on standard error
+                // output error stream
+                e.printStackTrace();
+            }
+        } else {
+            // Exception is handled, because to check
+            // whether the camera resource is being used by
+            // another service or not.
+            try {
+                // true sets the torch in OFF mode
+                cameraManager.setTorchMode(getCameraID, false);
+
+                // Inform the user about the flashlight
+                // status using Toast message
+                Toast.makeText(MainActivity.this, "Flashlight is turned OFF", Toast.LENGTH_SHORT).show();
+            } catch (CameraAccessException e) {
+                // prints stack trace on standard error
+                // output error stream
+                e.printStackTrace();
+            }
+        }
+    }
+}
